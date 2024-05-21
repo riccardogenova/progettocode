1b881537f67217a3c391e047920a93763e20cea7
Alessio <alessiobox18@gmail.com>
2024-05-21 12:33:23 +0200

    fix(config): fixbug

--- src/alessio_affatigato/website/js/esercizio10/script.js
+++ src/alessio_affatigato/website/js/esercizio10/script.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-document.addEventListener("DOMContentLoaded", function () {
-  var cane = document.getElementById("cane");
-  var bottone = document.getElementById("cambia");
-
-  bottone.addEventListener("click", function cambiaimmagine() {
-    if (cane.src.endsWith("cane_con_fucile.jpeg"))
-      cane.src = "cane_con_pistola.jpeg";
-    else {
-      cane.src = "cane_con_fucile.jpg";
-=======
 /** @format */
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -19,7 +8,6 @@ document.addEventListener('DOMContentLoaded', function () {
     if (cane.src.endsWith('cane_con_fucile.jpeg')) cane.src = 'cane_con_pistola.jpeg';
     else {
       cane.src = 'cane_con_fucile.jpg';
->>>>>>> f7e743fef29b4a744b1bd460b4833d16bbe37790
     }
   });
 });
